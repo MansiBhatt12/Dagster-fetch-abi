9deb7133ff98dbef0e723cbc38a6a28483e869b5
MansiBhatt12 <bhattmansi1212@gmail.com>
2023-07-04 12:42:02 +0530

    commit

---
+++ fetch_abi_csv_project/__init__.py
@@ -0,0 +1,56 @@
+from dagster import (
+    AssetSelection,
+    Definitions,
+    ScheduleDefinition,
+    sensor,
+    define_asset_job,
+    load_assets_from_modules,
+    FilesystemIOManager, 
+    RunRequest,
+    
+)
+
+
+from . import assets
+
+all_assets = load_assets_from_modules([assets])
+
+
+contract_job = define_asset_job("contract_job", selection=AssetSelection.all())
+
+contract_schedule = ScheduleDefinition(
+    job=contract_job,
+    cron_schedule="0 * * * *",  # every hour
+)
+
+
+@sensor(
+    name="contract_api_sensor",
+    asset_selection=AssetSelection.all(),
+    # interval=Interval(
+    #     seconds=30,
+    # ),
+)
+def contract_api_sensor():
+    # """A sensor that checks for changes to the contract API assets."""
+    # return RunRequest(
+    #     operation_id="contract_api_sensor",
+    #     assets_to_check=AssetSelection.all(),
+    # )
+    return RunRequest()
+
+
+io_manager = FilesystemIOManager(
+    base_dir="data",  # Path is built relative to where `dagster dev` is run
+)
+
+
+defs = Definitions(
+    assets=all_assets,
+    jobs=[contract_job], 
+    schedules=[contract_schedule],
+    resources={
+        "io_manager": io_manager,
+    },
+    sensors=[contract_api_sensor],  
+)
---
+++ fetch_abi_csv_project/assets.py
@@ -0,0 +1,35 @@
+import requests
+import csv
+
+from dagster import asset
+
+api_key = "M2Z69469HXICTNG2WJC9BC6IWECE3QY2K7"
+
+
+def fetch_api(contract_address) :
+    url = f"https://api.etherscan.io/api?module=contract&action=getsourcecode&address={contract_address}&apikey={api_key}"
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r.json()
+    
+
+@asset
+def contract_api_assets():
+    contract_addresses = []
+    with open("data/address.csv", "r") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            contract_addresses.append(row[0])
+
+    apis = []
+    for contract_address in contract_addresses:
+        api = fetch_api(contract_address)
+        if api is not None:
+            apis.append(api)
+    
+    with open("data/fetch_api.csv", "w") as csvfile:
+        writer = csv.writer(csvfile, delimiter=",")
+        for row in apis:
+            writer.writerow(row.values())
+
+    return apis
